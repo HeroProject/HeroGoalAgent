5e71354d0f3d13e9a6d33e69022a53e7251ce82f
kvhin <kvhin@192.168.56.1>
2019-06-23 09:50:18 +0200

    Updated version of our GOAL agent to control the NAO robot. Fixed several bugs, made state transition logic more robust, and cleaned up code by using several new Prolog definitions.

--- dialogflow.pl
+++ dialogflow.pl
@@ -1,44 +1,90 @@
-:-dynamic question/2, answer/2, event/1, waitingForEvent/1, timeout/1,
-	completed/1, nextCondition/1, currentState/1, waitingForAnswer/0, start/0,
-	currentAttempt/1.
+% Predicates for event percept processing. 
+:-dynamic answer/2, event/1.
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%% global (default) config parameters
-% give a user two tries to provide an answer to a question (of whatever type). [IDEA: Override by adding try=X option to question type states] 
-maxAnswerAttempts(1).
+% Predicates related to state execution and transition handling.
+:- dynamic currentAttempt/1, currentState/1, nextCondition/1, start/0, started/0, timeout/1, waitingForAnswer/0, waitingForEvent/1.
 
-% Time to answer question
-maxAnswerTime(4000).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% State completion logic               		   %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% A say state is completed if after starting it, all events that were started have been completed.
+% That is, a say state transitions from (1) start to (2) waiting for event completion ('TextDone', 'GestureDone', etc.) to (3) completion.
+completed(State) :- state(State, say), eventsCompleted.
+% A question state is completed if after starting it, all events that were started have been completed,
+% and an answer has been received, or a timeout occurred.
+% That is, a question state transitions from (1) start to (2) posing the question (waitingForEvent) to (3) waiting for answer to (4) complete.
+completed(State) :- state(State, question), eventsCompleted, answerReceived.
+
+% All events have been completed when all robot events indicating actions have been done (saying something, gesture, etc.) have been received.
+eventsCompleted :- started, not(waitingForEvent(_)).
+
+% An answer has been received when there is an answer and we're no longer waiting for an answer.
+answerReceived :- answer(_, _), not(waitingForAnswer).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% State parameter handling (incl. default config params) %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Returns values for all keys in list of condition Pairs in state.
+keyListValues(State, [Key | Keys], [Value | Values]) :- keyValue(State, Key, Value), keyListValues(State, Keys, Values).
+keyListValues(_, [], []).
+% Returns value for (one) key in list of condition Pairs in state.
+% (use of cut ! to prevent returning default values for keys).
+keyValue(State, Key, Value) :- stateConfig(State, Pairs), member((Key=Value), Pairs), !.
+
+%% Global (default) configuration parameters
+% (override config param for specific state by using key-label in key-value list associated with that state).
+% Number of tries a user gets to provide an answer to a question (of whatever type).
+keyValue(_, maxAnswerAttempts, 1).
+
+% Time (in milliseconds) a user gets to answer a question.
+keyValue(_, maxAnswerTime, 4000).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-state(s1, say, []).
-text(s1, "Hallo, ik ben Hero.", _).
+state(s1, say). % state s1 is of type 'say'.
+stateConfig(s1, []). % no configuration parameters for state s1 (empty list); if empty, no need to include stateConfig/2 for s1.
+text(s1, "Hallo, ik ben Hero.", []).
+anim(s1, 'animations/Stand/Gestures/Hey_1', []).
+leds(s1, 'white', []).
 
-state(s2, question, [type=yesno, response=speech, context='answer_yesno']).
-text(s2, "Hou je van chocola?", _).
+state(s2, question).
+stateConfig(s2, [type=yesno, response=speech, context='answer_yesno']).
+text(s2, "Hou je van chocola?", []).
 
 % In second instance try touch (feet bumpers)
-state(s2f, question, [type=yesno, response=touch]).
-text(s2f, "Sorry ik verstond je niet goed. Kun je daarom via de bumpers antwoord geven. Hou je van chocola?", _).
+state(s2f, question).
+stateConfig(s2f, [type=yesno, response=touch]).
+text(s2f, "Sorry ik verstond je niet goed. Kun je daarom via de bumpers antwoord geven. Hou je van chocola?", []).
 
-state(s3a, say, []).
-text(s3a, "Ik houd ook van chocola!", _).
+state(s3a, say).
+text(s3a, "Ik houd ook van chocola!", []).
 
-state(s3b, say, []).
-text(s3b, "Ik vind chocola ook vies!", _).
+state(s3b, say).
+text(s3b, "Ik vind chocola ook vies!", []).
 
-state(s4, say, []).
-text(s4, "Dat was het. Doei!", _).
+state(s4, question).
+stateConfig(s4, [type=mc, response=speech, context='answer_color']).
+text(s4, "Wat is jouw lievelingskleur?", []).
 
-next(s1, _, s2).
+state(s5, say).
+% TODO: variabelen.
+text(s5, "Die kleur vind ik ook heel mooi!", []).
+
+state(s6, say).
+text(s6, "Dat was het. Doei!", []).
+
+next(s1, 'true', s2).
 next(s2, 'answer_yes', s3a).
 next(s2, 'answer_no', s3b).
 next(s2, 'fail', s2f).
 next(s2f, 'answer_yes', s3a).
 next(s2f, 'answer_no', s3b).
 next(s2f, 'fail', s4).
-next(s3a, _, s4).
-next(s3b, _, s4).
+next(s3a, 'true', s4).
+next(s3b, 'true', s4).
+next(s4, 'true', s5).
+next(s5, 'true', s6).
 
-%state(s4, question, [type=mc, response=speech, context='answer_pets']).
-%state(s4a, question, [type=mc, response=touch, options=['hond', 'kat', 'konijn', 'cavia']]).
+%state(s5, question, [type=mc, response=speech, context='answer_pets']).
+%state(s5a, question, [type=mc, response=touch, options=['hond', 'kat', 'konijn', 'cavia']]).
