43c7492b632d67cd1f3e20fbca158f896568a2a0
Vincent Koeman <koeman@chainels.com>
2021-05-25 10:31:47 +0200

    put in the co-creation (?)

--- topics/hospital/slaapquiz.pl
+++ topics/hospital/slaapquiz.pl
@@ -291,31 +291,55 @@ text(routine,  s3nt3, "Als je dat een paar avonden doet, zal je merken dat het m
 %%% Pauze co-creating                                      %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-state(pauze1, s1, say).
-leds(pauze1,  s1, direct, ['FaceLeds', 'ChestLeds'], ['white', 'white']).
-text(pauze1,  s1, "Zo, we hebben de eerste twee onderwerpen gehad; laten we even pauze houden.").
-next(pauze1,  s1, "true",  s2).
+state(pauze1a, s1, say).
+leds(pauze1a,  s1, direct, ['FaceLeds', 'ChestLeds'], ['white', 'white']).
+text(pauze1a,  s1, "Zo, we hebben de eerste twee onderwerpen gehad; laten we even pauze houden.").
+next(pauze1a,  s1, "true",  s2).
 
-state(pauze1, s2, say).
-text(pauze1,  s2, "Misschien dat je mij wilt helpen om wat geluiden te maken, die kan ik straks namelijk goed gebruiken bij mijn uitleg; ga maar even staan!").
-next(pauze1,  s2, "true",  s3).
+state(pauze1a, s2, say).
+text(pauze1a,  s2, "Misschien dat je mij wilt helpen om wat geluiden te maken, die kan ik straks namelijk goed gebruiken bij mijn uitleg; ga maar even staan!").
+next(pauze1a,  s2, "true",  s3).
+
+state(pauze1a, s3, say).
+text(pauze1a,  s3, "Ik ben op zoek naar een goed applaus.").
+next(pauze1a,  s3, "true",  s4).
+
+state(pauze1a, s4, say).
+prepare_sound_anim(pauze1a, s4).
+stateConfig(pauze1a, s4, [umVariable=sound_applause, option1='resources/sounds/applaus.wav', option2='resources/sounds/applaus.wav', recordTime=3000]).
 
 % TODO: record applause, cheer, and snore
 % Kun je misschien voor mij applaudisseren? Doe maar in 3, 2, 1. Goed gedaan!
 % En kun je ook juichen, alsof je net een spelletje gewonnen hebt? Doe maar in 3, 2, 1. Super!
 % En als laatste: kun je eens laten horen hoe jij snurkt? Doe maar in 3, 2, 1. Haha, dat is grappig!
 
-state(pauze1, s3, say).
-text(pauze1,  s3, "Ik kan ook goed snurken, hoor maar!").
-next(pauze1,  s3, "true",  s4).
+state(pauze1b, s1, say).
+text(pauze1b,  s1, "Goed gedaan! Ik ben ook op zoek naar iemand die juicht, alsof je net een spelletje gewonnen hebt.").
+next(pauze1b,  s1, "true",  s2).
 
-state(pauze1, s4, say).
-audio(pauze1, s4, file, "resources/sounds/tiger2.wav"). % TODO: actual sound
-%anim(pauze1,  s4, file, "resources/gestures/racecar.xml"). % TODO: anim needed?
-next(pauze1,  s4, "true",  s5).
+state(pauze1b, s2, say).
+prepare_sound_anim(pauze1b, s2).
+stateConfig(pauze1b, s2, [umVariable=sound_cheer, option1='resources/sounds/lion1.wav', option2='resources/sounds/lion2.wav', recordTime=3000]). % TODO: sounds
 
-state(pauze1, s5, say).
-text(pauze1,  s5, "Haha, we kunnen allebei goed snurken; laten we verder gaan.").
+state(pauze1c, s1, say).
+text(pauze1c,  s1, "Super! Als laatste ben ik op zoek naar een goed snurk geluid.").
+next(pauze1c,  s1, "true",  s2).
+
+state(pauze1c, s2, say).
+prepare_sound_anim(pauze1c, s2).
+stateConfig(pauze1c, s2, [umVariable=sound_snore, option1='resources/sounds/tiger1.wav', option2='resources/sounds/tiger2.wav', recordTime=3000]). % TODO: sounds
+
+state(pauze1d, s1, say).
+text(pauze1d,  s1, "Ik kan ook goed snurken, hoor maar!").
+next(pauze1d,  s1, "true",  s2).
+
+state(pauze1d, s2, say).
+audio(pauze1d, s2, file, "resources/sounds/tiger2.wav"). % TODO: actual sound
+%anim(pauze1d,  s2, file, "resources/gestures/racecar.xml"). % TODO: anim needed?
+next(pauze1d,  s2, "true",  s3).
+
+state(pauze1d, s3, say).
+text(pauze1d,  s3, "Haha, we kunnen allebei goed snurken; laten we verder gaan.").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Slaapkamer comfortabel                                 %%%
@@ -386,7 +410,7 @@ text(slaapkamer,  s2i2, "Die slaapstofjes helpen je lichaam om beter te slapen."
 next(slaapkamer,  s2i2, "true",  s2i3).
 
 state(slaapkamer, s2i3, say).
-audio(slaapkamer, s2i3, file, "resources/sounds/tiger2.wav"). % TODO: co-created snore?
+audio(slaapkamer, s2i3, id, sound_snore).
 %anim(slaapkamer,  s2i3, file, "resources/gestures/racecar.xml"). % TODO: anim needed?
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -471,7 +495,7 @@ next(pauze2,  s4, "true",  s5).
 
 state(pauze2, s5, say).
 text(pauze2,  s5, "Goed gedaan!").
-audio(pauze2, s5, file, "resources/sounds/applaus.wav"). % TODO: co-created applause?
+audio(pauze2, s5, id, sound_applause).
 %anim(pauze2,  s5, file, "resources/gestures/racecar.xml"). % TODO: anim needed?
 next(pauze2,  s5, "true",  s6).
 
@@ -519,7 +543,7 @@ text(bewegen,  s1i3, "Ik doe aan robot voetbal.").
 next(bewegen,  s1i3, "true", s1i4).
 
 state(bewegen, s1i4, say).
-audio(bewegen, s1i4, file, "resources/sounds/elephant.wav"). % TODO: co-created cheer?
+audio(bewegen, s1i4, id, sound_cheer).
 %anim(bewegen,  s1i4, file, "resources/gestures/racecar.xml"). % TODO: anim needed?
 next(bewegen,  s1i4, "true", s1i5).
 
