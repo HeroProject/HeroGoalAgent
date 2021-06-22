e3c1a974ae55eadc32765ef75e18dd6851d733aa
Vincent Koeman <koeman@chainels.com>
2021-06-22 12:10:40 +0200

    improve sorter layout

--- util/html.pl
+++ util/html.pl
@@ -44,11 +44,11 @@ buttons([],Html,Html).
 buttons([Curr|Rest],Tmp,Html) :- button(Curr,B), atom_concat(Tmp,B,New), buttons(Rest,New,Html).
 buttons(ContentList,Html) :- buttons(ContentList,'',Html).
 
-sortItem('<div id="~a" class="card sortitem"><div class="card-img-caption"><h1 class="card-text bg-secondary"></h1><img class="card-img-top" src="~a"></div></div>').
+sortItem('<div class="col mb-4"><div id="~a" class="card h-100 sortitem"><div class="card-img-caption"><h1 class="card-text bg-secondary"></h1><img class="card-img-top" src="~a"></div></div></div>').
 sortItem(Id=Src,Html) :- number(Src), character(C), character_images(C,L), nth1(Src,L,Img), 
 				sortItem(T), format(atom(Html),T,[Id,Img]).
 sortItem(Id=Src,Html) :- sortItem(T), format(atom(Html),T,[Id,Src]).
 
-sortItems([],Final,Html) :- format(atom(Html),'<div class="card-group">~a</div>',[Final]).
+sortItems([],Final,Html) :- format(atom(Html),'<div class="row row-cols-1 row-cols-md-4">~a</div>',[Final]).
 sortItems([Curr|Rest],Tmp,Html) :- sortItem(Curr,B), atom_concat(Tmp,B,New), sortItems(Rest,New,Html).
 sortItems(ContentList,Html) :- sortItems(ContentList,'',Html).
